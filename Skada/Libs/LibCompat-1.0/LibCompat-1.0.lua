5ec9c765ff3bfb0ceb9873868255c8d4c2e435e6
Andrew6810 <16847730+andrew6180@users.noreply.github.com>
2022-11-25 12:12:25 -0700

    fix lib.Ascension check

--- Skada/Libs/LibCompat-1.0/LibCompat-1.0.lua
+++ Skada/Libs/LibCompat-1.0/LibCompat-1.0.lua
@@ -540,7 +540,7 @@ do
 	local classColorsTable, classCoordsTable
 
 	-- flags for Projects Ascension
-	lib.Ascension = (type(C_Realm) == "function")
+	lib.Ascension = (type(C_Realm) == "table")
 	lib.AscensionCoA = lib.Ascension and C_Realm:IsConquestOfAzeroth()
 
 	local function GetClassColorsTable()
